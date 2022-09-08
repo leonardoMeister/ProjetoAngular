3671262ecec6303be69e8831268a70547551f419
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-09-08 14:55:43 -0300

    att

--- src/app/requisicoes/requisicao.module.ts
+++ src/app/requisicoes/requisicao.module.ts
@@ -6,11 +6,13 @@ import { RequisicaoComponent } from './requisicao.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { RequisicoesFuncionarioComponent } from './requisicoes-funcionario/requisicoes-funcionario.component';
 
 
 @NgModule({
     declarations: [
-        RequisicaoComponent
+        RequisicaoComponent,
+        RequisicoesFuncionarioComponent
     ],
     imports: [
         CommonModule,
---
+++ src/app/requisicoes/requisicoes-funcionario/requisicoes-funcionario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-requisicoes-funcionario',
+  templateUrl: './requisicoes-funcionario.component.html',
+  styleUrls: ['./requisicoes-funcionario.component.css']
+})
+export class RequisicoesFuncionarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
