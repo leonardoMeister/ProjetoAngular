d57ecf37048c860acba00f2711fedac6f4d6bcad
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-09-10 18:46:44 -0300

    config de view de tabela

--- src/app/requisicoes/requisicoes-departamento/requisicoes-departamento.component.ts
+++ src/app/requisicoes/requisicoes-departamento/requisicoes-departamento.component.ts
@@ -4,7 +4,12 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from 'src/app/auth/services/authentication.service';
+import { DepartamentoService } from 'src/app/departamentos/services/departamento.service';
+import { EquipamentoService } from 'src/app/equipamentos/service/equipamento.service';
+import { FuncionarioService } from 'src/app/funcionarios/services/funcionario.service';
+import { Movimentacao } from '../models/movimentacao.model';
 import { Requisicao } from '../models/requisicao.model';
+import { RequisicaoService } from '../services/requisicao.service';
 
 @Component({
     selector: 'app-requisicoes-departamento',
@@ -19,11 +24,19 @@ export class RequisicoesDepartamentoComponent implements OnInit {
         private fb: FormBuilder,
         private modalService: NgbModal,
         private toastr: ToastrService,
-        private authService: AuthenticationService
+        private authService: AuthenticationService,
+
+        private funcionarioService: FuncionarioService,
+        private departamentoService: DepartamentoService,
+        private equipamentoService: EquipamentoService,
+        private requisicoesService: RequisicaoService,
+
         ) { }
 
     ngOnInit(): void {
-        
+        this.requisicoes$ = this.requisicoesService.selecionarTodos();
+        let asd: Movimentacao[] = []
+        asd.length
     }
 
 }
