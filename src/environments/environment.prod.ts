c8b51515cb105e2b6871b75071ea24ecd673c619
Leonardo Margoti Meister <leozinhomm1@gmail.com>
2022-09-13 16:24:21 -0300

    att

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDHyf8fAVKJWY_cPCrsrleBTYbKHZIJMeU",
+    authDomain: "requisicoes-app-d264a.firebaseapp.com",
+    projectId: "requisicoes-app-d264a",
+    storageBucket: "requisicoes-app-d264a.appspot.com",
+    messagingSenderId: "659713503188",
+    appId: "1:659713503188:web:830cc8757fb280593ab041",
+    measurementId: "G-697C6CQ7JS"
+  }
 };
